7afeab26cd1b7f28f1fa352a40b2a8a896ea513a
Senuk thiyasara <96465619+Senuk27@users.noreply.github.com>
2023-04-03 15:23:23 +0530

    Add project files.

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login.Models;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com= new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString() 
+        {
+            con.ConnectionString = "data source= DESKTOP-GHTOG0T; database=SA ; integrated security=SSPI;";
+        }
+        public ActionResult Verify(Account acc) 
+        {
+            connectionString();
+            con.Open();
+            com.Connection= con;
+            com.CommandText = "select * from [dbo].[login] where Username='"+acc.name+"' and Password='"+acc.password+"'";
+            dr= com.ExecuteReader();
+
+            if(dr.Read()) 
+            {
+                con.Close();
+                return View("create");
+            }
+            else 
+            {
+                con.Close();
+                return View("Error");
+            }    
+            
+
+        
+           
+           
+        }
+    }
+}
